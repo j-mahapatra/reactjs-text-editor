a98ab3bb2935a4b261a0f01a781c96a1da8b49a9
j-mahapatra <jagannathm.official@gmail.com>
2024-01-15 14:19:12 +0530

    feat: create header of the app

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import '../styles/header.css';
+
+export default function Header() {
+  return (
+    <div className='header'>
+      <h1 className='header__title'>Demo Editor by Jagannath Mahapatra</h1>
+      <button className='header__button'>Save</button>
+    </div>
+  );
+}
